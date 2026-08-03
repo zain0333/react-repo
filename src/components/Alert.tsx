fab139463fbc393b4c9ef6071eac5989e38815e5
zain0333 <zain03337631969@gmail.com>
2026-08-03 01:50:28 -0700

    add alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,5 +1,9 @@
-const Alert = () => {
-  return <div className="alert alert-primary">Alert</div>;
+interface Props {
+  text: string;
+}
+
+const Alert = ({ text }: Props) => {
+  return <div className="alert alert-primary">{text}</div>;
 };
 
 export default Alert;
