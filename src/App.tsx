0148d022d0fadafb12486cf4fa4763b954ed5d4a
zain0333 <zain03337631969@gmail.com>
2026-08-03 00:48:06 -0700

    another list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import ListGroup from './components/ListGroup'; 
+import ListGroup from "./components/ListGroup";
 
 function App() {
-   return (
-      <div><ListGroup /><ListGroup</div>
-   );
+  return (
+    <div>
+      <ListGroup />
+      <ListGroup />
+    </div>
+  );
 }
 
 export default App;
