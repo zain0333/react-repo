a8941950415a97d759f608786c6e3bb9ad6d0880
zain0333 <zain03337631969@gmail.com>
2026-08-03 00:00:10 -0700

    commit for managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -14,7 +14,11 @@ function ListGroup() {
       {items.length === 0 && <p>No items found.</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className="list-group-item active"
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
