b19ac30b24db815f6cd792267077ff6eaedb82c4
zain0333 <zain03337631969@gmail.com>
2026-08-02 23:38:33 -0700

    commit for index

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -8,11 +8,11 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>No items found.</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log(item)}
+            onClick={() => console.log(item, index)}
           >
             {item}
           </li>
