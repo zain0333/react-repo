3922398b380c61dc34e8f21424be77b3ae4e31b5
zain0333 <zain03337631969@gmail.com>
2026-08-03 00:45:21 -0700

    add another list

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import ListGroup from './components/ListGroup';
 
 function App() {
    return (
-      <div>
-         <ListGroup />
-      </div>
+      <div><ListGroup /><ListGroup</div>
    );
 }
 
